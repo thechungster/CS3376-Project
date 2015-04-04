0e360cb451265c525a4c4b26dd45edd6cb83c136
thechungster <timmmmy400@gmail.com>
2015-04-03 19:46:29 -0500

    finished bool source

--- BoolSource.cpp
+++ BoolSource.cpp
@@ -2,5 +2,12 @@
 using namespace std;
 
 class BoolSource()	{
-	
+
+		//have some algorithm to check if plane should enter landing queue
+		return false;
+	}
+
+		//have some algorithm to check of plane should enter takeoff queue
+		return false;
+	}
 }
