563271da4d3f7b99dbee9a9aa4d7721fe919a280
thechungster <timmmmy400@gmail.com>
2015-04-03 19:47:24 -0500

    finished bool source
    i think

--- BoolSource.cpp
+++ BoolSource.cpp
@@ -3,10 +3,12 @@ using namespace std;
 
 class BoolSource()	{
 
+	public boolean enterLandingQueue()	{
 		//have some algorithm to check if plane should enter landing queue
 		return false;
 	}
 
+	public boolean enterTakeoffQueue()	{
 		//have some algorithm to check of plane should enter takeoff queue
 		return false;
 	}
