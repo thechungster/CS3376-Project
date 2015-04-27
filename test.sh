28c503793880e2faf880f3f5e56f643388f31c6a
Richard Ho <richardho94@gmail.com>
2015-04-26 19:00:58 -0500

    shell file created

---
+++ test.sh
@@ -0,0 +1,7 @@
+./Simulation 5 7 .5 .4 360 0 20
+./Simulation 3 3 .5 .5 720 0 30
+./Simulation 4 8 .3 .3 360 0 20
+./Simulation 4 3 .25 .25 720 0 30
+./Simulation 3 4 .10 .10 360 0 60
+./Simulation 2 2 .05 .05 360 0 60
+./Simulation 2 2 .50 .05 360 0 120
