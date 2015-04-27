d5da5ebc6f0135c91899dab4f88be5a607649f3c
Richard Ho <richardho94@gmail.com>
2015-04-26 22:19:20 -0500

    updated script

--- test.sh
+++ test.sh
@@ -1,6 +1,4 @@
-./Simulation 5 7 .5 .4 360 0 20
-./Simulation 5 7 .3 .3 360 0 20
-./Simulation 5 7 .10 .10 360 0 20
-./Simulation 5 7 .05 .05 360 0 20
-./Simulation 5 7 .50 .05 360 0 20
-./Simulation 5 7 .05 .50 360 0 20
+./airsim 5 7 .5 .4 360 0 20
+./airsim 5 7 .7 .6 360 0 20
+./airsim 5 7 .25 .2 360 0 20
+./airsim 5 7 .125 .25 360 0 20
